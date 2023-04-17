531578b227469954accd17b1877736b3b189b499
Muhammad Rehan <66232567+mxrehan@users.noreply.github.com>
2023-04-17 12:20:51 -0700

    Update prismacloud_account_group.tf

--- prismacloud_account_group.tf
+++ prismacloud_account_group.tf
@@ -1,10 +1,9 @@
 # Manage an account group. This TF will create an account group.
-
 terraform {
   required_providers {
     prismacloud = {
       source = "PaloAltoNetworks/prismacloud"
-      version = "1.2.10"
+      version = "1.3.1"
     }
   }
 }
@@ -13,11 +12,28 @@ provider "prismacloud" {
     url = var.url
     username = var.username
     password = var.password
+#     {
+#     "url" : "<PRISMA_CLOUD_API_URL",
+#     "username" : "<PRISMA_CLOUD_ACCESS_KEY>",
+#     "password" : "<PRISMA_CLOUD_ACCESS_SECRET>"
+# }
 }
 
+# locals {
+#     account_groups = csvdecode(file("acc_grps.csv"))
+# }
+
+# // Now specify the account group resource with a loop like this:
+# resource "prismacloud_account_group" "example" {
+#         for_each = { for inst in local.account_groups : inst.name => inst }
+
+#         name = each.value.name
+#         account_ids = split("||", each.value.accountIDs)
+#         description = each.value.description
+# }
 
 resource "prismacloud_account_group" "rehan-test-accountgroup" {
     name = "rehan-test-accountgroup"
     description = "Made by Terraform"
-    account_ids = [ accountid1,accountid2 ]
+    account_ids = [ 212193468572 ]
 }
