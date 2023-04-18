de76adbbb1a489554c5c7102d1d22fb1b9d5a628
Muhammad Rehan <66232567+mxrehan@users.noreply.github.com>
2023-04-17 17:31:12 -0700

    Update prismacloud_alert_rule.tf

--- prismacloud_alert_rule.tf
+++ prismacloud_alert_rule.tf
@@ -1,10 +1,9 @@
 # Manage an alert rule. This TF will create an alert rule with multiple account groups.
-
 terraform {
   required_providers {
     prismacloud = {
       source = "PaloAltoNetworks/prismacloud"
-      version = "1.2.10"
+      version = "1.3.1"
     }
   }
 }
@@ -15,13 +14,13 @@ provider "prismacloud" {
     password = var.password
 }
 
-
 resource "prismacloud_alert_rule" "Rehan-test-alert-rule" {
     name = "Rehan-test-alert-rule"
     description = "Made by Terraform"
+    allow_auto_remediate = false
     enabled = "true"
     scan_all = true
     target {
-    account_groups = ["accountgroup1", "accountgroup2" ]
+    account_groups = ["8501242b-f06d-4048-bb25-7e75aa11602c" ]
     }
 }
